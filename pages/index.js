b2ce3e3800c33871848125ae9642cd729be266d2
Alvin Cho <alvin.cho22@gmail.com>
2022-08-01 00:07:48 -0700

    added collapsle
    using npm module for collapsing

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import styles from '../styles/Home.module.css'
 import utilsSytles from '../styles/utils.module.css'
 import alertStyles from '../styles/alert.module.css'
 import useSWR from 'swr'
+import Collapsible from 'react-collapsible';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
@@ -22,7 +23,7 @@ function Healthcheck() {
   console.log(data)
 
   var colorStyle = 'color: red';
-  var statusHTML = '';
+  var statusHTML = [];
 
   if (data.length <= 0)
     return (
@@ -33,16 +34,22 @@ function Healthcheck() {
 
   for (let i = 0; i < data.length; i++) {
     //console.log(data[i].name);
+    if (!(data[i].name).includes('Tenant'))
+    {
+      statusHTML.push(<h1>{data[i].name}</h1>)
+      statusHTML.push(<h1 className={data[i].status == 'STATUS_OK' ? alertStyles.success :  alertStyles.error}>{data[i].status}</h1>)
 
-    statusHTML += (<h1> + data[i].name + </h1>)
+      var image = '/images/error.png'
 
-    if (data[i].status == 'STATUS_OK')
-      colorStyle = 'color: green';
-    else
-      colorStyle = 'color: red';
-    
-    //statusHTML += <h1 className={colorStyle}> + data[i].status + </h1>;
+      if (data[i].status == 'STATUS_OK')
+        image = '/images/success.png'
+
+      statusHTML.push(<Image alt='Success' src={image} height='36' width='36' />)
+    }
   }
+
+  var temp = [];
+  temp.push(<h1>temp</h1>);
 
   var statusHTML2 = (
     <div>
@@ -54,19 +61,39 @@ function Healthcheck() {
 
       <h1 className={utilsSytles.headingMd}>Features</h1>
       <h1>
+        <div className={data[2].status == 'STATUS_OK2' ? alertStyles.success : alertStyles.error}>Accounts</div>
+        &nbsp; &nbsp; Status
+        &nbsp; &nbsp; <Image alt='Error' src="/images/error.png" height='36' width='36' />
+      </h1>
+      <h1>
         <div className={data[2].status == 'STATUS_OK' ? alertStyles.success : alertStyles.error}>Search</div>
         &nbsp; &nbsp; Status
         &nbsp; &nbsp; <Image alt='Error' src="/images/error.png" height='36' width='36' />
       </h1>
-
-
     </div>
   )
 
   return (
     <div>
-      {statusHTML}
       {statusHTML2}
+
+      <div>
+        <Collapsible trigger="Nerdy Details" className={utilsSytles.heading2Xl}>
+          <h1>
+            <div className={data[4].status == 'STATUS_OK2' ? alertStyles.success : alertStyles.error}>Marketplace Integration</div>
+            &nbsp; &nbsp; Status
+            &nbsp; &nbsp; <Image alt='Error' src="/images/error.png" height='36' width='36' />
+          </h1>
+
+          <h1>
+            <div className={data[3].status == 'STATUS_OK' ? alertStyles.success : alertStyles.error}>Redis</div>
+            &nbsp; &nbsp; Status
+            &nbsp; &nbsp; <Image alt='Error' src="/images/success.png" height='36' width='36' />
+          </h1>
+
+          {statusHTML}
+        </Collapsible>
+      </div>
     </div>
   )
 }
